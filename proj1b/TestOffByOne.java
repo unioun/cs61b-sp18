8b19740d2fa3e1606a08f1abe4cc40d71731e00f
unioun <ciefm@outlook.com>
2023-04-27 10:58:11 +0800

    for GradeScope's test

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -4,18 +4,16 @@ import static org.junit.Assert.*;
 public class TestOffByOne {
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
-    public static CharacterComparator offByOne;
+    public static CharacterComparator offByOne = new OffByOne();
 
     @Test
     public void testOffByOne() throws Exception {
-        OffByOne check = new OffByOne();
-        assertTrue(check.equalChars('a', 'b'));
-        assertTrue(check.equalChars('r', 'q'));
-        assertTrue(check.equalChars('&', '%'));
-
-        assertFalse(check.equalChars('a', 'e'));
-        assertFalse(check.equalChars('z', 'a'));
-        assertFalse(check.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
     }
 
 }
