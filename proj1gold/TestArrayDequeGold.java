602ba3d304c3d5c7dddc73d38e820931af3045ed
unioun <ciefm@outlook.com>
2023-04-28 17:24:57 +0800

    For gradescope

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -122,41 +122,27 @@ public class TestArrayDequeGold {
     public void randomTest() {
         ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
-        int time = 10000;
-        int randomIndex = 0;
+        int time = 100000;
         String message = "";
         for (int i = 0; i < time; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (sol.size() > 0) {
-                randomIndex = StdRandom.uniform(sol.size());
-            }
             if (numberBetweenZeroAndOne < 0.25) {
                 sol.addFirst(i);
                 stu.addFirst(i);
-                message = message + " addFirst(" + i + ")\n";
+                message = message + "addFirst(" + i + ")\n";
             } else if (numberBetweenZeroAndOne >= 0.25 && numberBetweenZeroAndOne < 0.5) {
                 sol.addLast(i);
                 stu.addLast(i);
-                message = message + " addLast(" + i + ")\n";
+                message = message + "addLast(" + i + ")\n";
             } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
                 if (sol.size() > 0 && stu.size() > 0) {
-                    Integer removeItem = stu.removeFirst();
-                    if (removeItem == null) {
-                        message = message + " removeFirst(): " + "null" + "\n";
-                    } else {
-                        message = message + " removeFirst(): " + removeItem.intValue() + "\n";
-                    }
-                    assertEquals(message, sol.removeFirst(), removeItem);
+                    message = message + "removeFirst()\n";
+                    assertEquals(message, sol.removeFirst(), stu.removeFirst());
                 }
             } else {
                 if (sol.size() > 0 && stu.size() > 0) {
-                    Integer removeItem = stu.removeLast();
-                    if (removeItem == null) {
-                        message = message + " removeLast(): " + "null" + "\n";
-                    } else {
-                        message = message + " removeLast(): " + removeItem.intValue() + "\n";
-                    }
-                    assertEquals(message, sol.removeLast(), removeItem);
+                    message = message + "removeLast()\n";
+                    assertEquals(message, sol.removeLast(), stu.removeFirst());
                 }
             }
         }
